98224fa0da1027f61e28448bd34d094183546937
Vlad <72874472+wazzupbruh@users.noreply.github.com>
2021-07-18 16:25:27 +0300

    Update Task4.js

--- Classes/Task4.js
+++ Classes/Task4.js
@@ -19,3 +19,5 @@ class Dog extends Animal {
   introduce(){ return `${super.introduce()}`;}
   greetMaster(){ return `Hello ${this.master}`}
 }
+
+// https://www.codewars.com/kata/56f935002e6c0d55fa000d92
