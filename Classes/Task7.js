e4148ab66d3f7f58ead13000c8922149762b4dc7
Vlad <72874472+wazzupbruh@users.noreply.github.com>
2021-07-22 23:26:28 +0300

    Update Task7.js

--- Classes/Task7.js
+++ Classes/Task7.js
@@ -2,4 +2,4 @@ const task = () => setTimeout(() => console.log('Test'), 7000)
 
 const antiOptimizeAsync = (task) => new Promise((resolve) => setTimeout(() => resolve(task()), 1500))
 
-// https://www.codewars.com/kata/58d014421c694f71630000bc/train/javascript
+// https://www.codewars.com/kata/58d014421c694f71630000bc
