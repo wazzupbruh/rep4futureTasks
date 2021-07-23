ac9fa4c28dba4e13c5fe16ac1183e06406e43e19
Vlad <72874472+wazzupbruh@users.noreply.github.com>
2021-07-23 03:42:44 +0300

    Update 02-consuming-promises.js

--- promises-exercises/02-consuming-promises.js
+++ promises-exercises/02-consuming-promises.js
@@ -1,1 +1,12 @@
+//ex1
+let promise = new Promise(resolve => setTimeout(() => resolve('promise'), 100))
+
+let action = (str) => console.log(str + ' has been performed')
+
+const waitForPromise = async (promise, action) => {
+  let a = await promise;
+  await action(a)
+}
+
+//ex2
 
