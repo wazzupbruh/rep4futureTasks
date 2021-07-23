8479f4ac874c6cdaf6bceb6fddb3489261d97df7
Vlad <72874472+wazzupbruh@users.noreply.github.com>
2021-07-23 10:28:00 +0300

    Update 01-making-promises.js

--- promises-exercises/01-making-promises.js
+++ promises-exercises/01-making-promises.js
@@ -5,7 +5,7 @@ const makePromiseResolveWith3 = () => Promise.resolve(3)
 const makePromiseRejectWithBoo = () => Promise.reject('Boo!')
 
 //ex3
-const makePromiseWithConstructor = (itShouldResolve) => new Promise((resolve, reject) => itShouldResolve ? resolve() : reject());
+const makePromiseWithConstructor = (itShouldResolve) => new Promise((resolve, reject) => !!itShouldResolve ? resolve() : reject());
 
 //ex4
 const makeDelayPromise = (value, delayInMs) => new Promise((resolve) => setTimeout(() => resolve(value), delayInMs))
