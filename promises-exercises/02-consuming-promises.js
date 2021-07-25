c43e87ee04d10bdc0a44e322f529cfae70c27359
Vlad <72874472+wazzupbruh@users.noreply.github.com>
2021-07-26 00:09:39 +0300

    Update 02-consuming-promises.js

--- promises-exercises/02-consuming-promises.js
+++ promises-exercises/02-consuming-promises.js
@@ -1,21 +1,11 @@
 //ex1
-let promise = new Promise(resolve => setTimeout(() => resolve('promise'), 100))
-let action = (str) => console.log(str + ' has been performed')
 const waitForPromise = async (promise, action) => {
-  let a = await promise;
-  await action(a)
+   let a = await promise;
+   await action(a)
 }
-console.log(waitForPromise(promise, action))
 
 //ex2
-let promise = new Promise((resolve) => resolve('resolve')), consumer = (str) => console.log(str + ' consumer'), handler = 'an error'
-const consumePromise = async (promise, consumer, handler) => {
-  try{
-    let a = await promise
-    consumer(a)
-  }catch(handler){
-    console.log(handler)
-  }
-}
-console.log(consumePromise(promise, consumer, handler))
+const consumePromise = (promise, consumer, handler) => {
+  promise.then(result => consumer(result)).catch(err => handler(err))
+})
 
