682239c380e8acde0f98f54496a4852c4f76f9c1
Vlad <72874472+wazzupbruh@users.noreply.github.com>
2021-07-26 00:11:55 +0300

    Update 02-consuming-promises.js

--- promises-exercises/02-consuming-promises.js
+++ promises-exercises/02-consuming-promises.js
@@ -5,5 +5,7 @@ const waitForPromise = async (promise, action) => {
 }
 
 //ex2
-const consumePromise = (promise, consumer, handler) => promise.then(result => consumer(result)).catch(err => handler(err)))
+const consumePromise = (promise, consumer, handler) => {
+  promise.then(result => consumer(result)).catch(err => handler(err))
+ }
 
